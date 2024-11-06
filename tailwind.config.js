e1696f70fdebb43ceaa9c84c3677fa1e0490b9c7
mbido <ma.bidault39@gmail.com>
2024-11-06 11:07:10 +0100

    basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#2A2A2A',
+        'secondary': '#4A5568',
+      }
+    }
+  },
+  plugins: [],
+}
